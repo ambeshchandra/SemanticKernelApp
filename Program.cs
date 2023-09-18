e791112dc704a2aed43116718647da31825756c4
ambeshchandra <ambeshchandra@gmail.com>
2023-09-18 11:49:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myStr = "kkllmmnnoo";
+Console.WriteLine("Initial String: " + myStr);
+var unique = new HashSet<char>(myStr);
+Console.Write("New String after removing duplicates: ");
+foreach (char c in unique)
+    Console.Write(c);
